1f9a728542f0576da6fbe2a13edacd89a256c9aa
jfjubayer <jubayerjf28@gmail.com>
2026-07-08 20:46:56 +0600

    added dhrystone test

--- sim/top.sv
+++ sim/top.sv
@@ -84,8 +84,8 @@ module top;
         $dumpfile("sim.fst");
         $dumpvars;
 
-        // Run for 10000 cycles max
-        repeat (100000) @(negedge clk);
+        // Run for 10000000 cycles max
+        repeat (10000000) @(negedge clk);
 
         // Stop simulation
         $display("\033[0;33m"); // color_orange
